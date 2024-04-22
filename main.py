fd4291bb145dc550ada9f6a56eacc36552a9e41c
yash-raj202134 <yashraj3376@gmail.com>
2024-04-22 23:58:06 +0530

    utils update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
 from utils.video_utils import read_video,write_video
-
+from trackers.tracker import Tracker
 
 def main():
     
     # read video
     video_frame = read_video("input_videos/08fd33_4.mp4")
+
+    # Initialize tracker
+
+    tracker = Tracker("models/best.pt")
+    tracks = tracker.get_object_tracks(video_frame,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
 
     # save video
     write_video(video_frame,'output_videos/output_video.avi')
