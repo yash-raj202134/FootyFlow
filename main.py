7251db17a8b07c295613a0b81a4b94f9e1359ef8
yash-raj202134 <yashraj3376@gmail.com>
2024-04-23 15:47:17 +0530

    assigned team color update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from utils.video_utils import read_video,write_video
 from trackers.tracker import Tracker
+from team_assigner.team_assigner import TeamAssigner
+
 
 def main():
     
@@ -11,6 +13,23 @@ def main():
 
     tracker = Tracker("models/best.pt")
     tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
+
+
+
+    # Assign player teams
+
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0],tracks['players'][0])
+
+    for frame_num , player_track in enumerate(tracks['players']):
+        for player_id , track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],track['bbox'],player_id)
+
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+            
+
+            
     
 
     # Draw output 
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -182,7 +182,8 @@ class Tracker:
 
             # Draw players
             for track_id , player in player_dict.items():
-                frame = self.draw_ellipse(frame, player["bbox"],(0,0,225), track_id)
+                color = player.get("team_color",(0,0,225))
+                frame = self.draw_ellipse(frame, player["bbox"],color, track_id)
 
             # Draw referees
             for _ , referee in referee_dict.items():
